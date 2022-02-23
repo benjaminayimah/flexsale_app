a6df9af8a57ce0fd45a27379af734ddc9b5c2ef6
Benjamin <benjaminayimah@gmail.com>
2022-02-23 14:34:22 +0000

    login update

--- src/views/web/Login.vue
+++ src/views/web/Login.vue
@@ -85,8 +85,8 @@ export default {
     data() {
         return {
             form: {
-              email: 'benjaminayimah@gmail.com',
-              password: 'ajf1432#@!'
+              email: '',
+              password: ''
             },
             validation: {
                 error: false,
