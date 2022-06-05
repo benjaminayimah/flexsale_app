fc1257b91d079136b8d03ecba460f1bf928bc7c8
Benjamin <benjaminayimah@gmail.com>
2022-06-05 18:03:53 +0000

    search feature

---
+++ src/views/app/Search.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="main-page-body">
+       <div>
+           <div class="sticky-top flex align-items-center">
+               <!-- <search-float /> -->
+           </div>
+           <div style="height:700px">
+                <!-- body -->
+                <div>dfdfddf</div>
+                <div>dfdfd</div>
+                <div>dfdfd</div>
+
+            </div>
+       </div>
+    </div>
+</template>
+<script>
+// import SearchFloat from '../../components/app/includes/SearchFloat.vue'
+export default {
+//   components: { SearchFloat },
+    name: 'Search',
+    created() {
+        window.scrollTo(0,0)
+        this.setPage()
+    },
+    methods: {
+        setPage() {
+            const title = { title: 'Search', back: true}
+            this.$store.commit('setPagetitle', title)
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+.sticky-top{
+    top: 80px;
+    height: 100px;
+    background-color: #ffffff;
+}
+
+</style>
