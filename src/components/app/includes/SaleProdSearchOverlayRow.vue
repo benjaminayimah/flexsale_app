6961d7040960a36f36c6a8ed85c6eee47891ac29
Benjamin <benjaminayimah@gmail.com>
2022-08-13 13:57:44 +0000

    release beta 2.2

--- src/components/app/includes/SaleProdSearchOverlayRow.vue
+++ src/components/app/includes/SaleProdSearchOverlayRow.vue
@@ -6,19 +6,14 @@
                 <div class="prd-img bg-img" :style="product.image ? { backgroundImage: 'url('+getHostname+'/storage/'+getUserAdminID+'/'+getUser.current+'/'+product.image+')'} : { backgroundImage: 'url('+getDefaultImage+')'}"></div>
                 <div class="prod-captions">
                     <div class="item-name">{{ product.name }}</div>
-                    <div class="flex">
-                        <label>Price:</label>
-                        <div :class="{ 'has-discount': product.discount !== null && product.selling_price != 0 &&  computePrice !== 0  }"><span class="currency">{{ getCurrency }}</span>{{ product.selling_price }}</div>
-                        <div class="discount-price" v-if="product.discount !== null && product.selling_price != 0 &&  computePrice !== 0"><span class="currency">{{ getCurrency }}</span><span>{{ Intl.NumberFormat('en-US').format(computePrice.toFixed(2)) }}</span></div>
-                    </div>
                     <div><label>Stock:</label><span>{{ product.stock }}</span></div>
-                </div>
-            </div>
-            <div class="flex gap-4">
-                <div class="flex align-items-center gap-4" v-if="!computeExpiry">
-                    <label>Qty:</label>
+                    <div class="flex align-items-center gap-4" v-if="!computeExpiry">
+                        <label>Qty:</label>
                     <input type="number" v-model="quantity" class="form-control qty-input">
                 </div>
+                </div>
+            </div>
+            <div class="flex gap-4" :class="{ 'flex-col' : getMobile }">
                 <button class="button uppercase gap-4" @click="fetchProduct">
                     <svg v-if="!submitting" xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11.41 11.41">
                         <path d="M-16851.348,10385.642h-2v-4.7h-4.707v-2h4.707v-4.707h2v4.707h4.7v2h-4.7Z" transform="translate(16858.055 -10374.231)" fill="#566ff4"/>
@@ -27,7 +22,6 @@
                     <span>add</span>
                 </button>
             </div>
-            
         </div>
     </li>
 </template>
@@ -47,25 +41,7 @@ export default {
         };
     },
     computed: {
-        ...mapGetters(["getHostname", "getToken", "getUser", "getDefaultImage", "getDiscounts", "getCurrency", "getUserAdminID"]),
-        computeDiscount: function () {
-            return this.getDiscounts.find(discount => discount.id == this.product.discount);
-        },
-        computePrice() {
-            if (this.product.discount !== null && this.computeDiscount && this.computeDiscount.active == 1) {
-                if (this.computeDiscount.percentage == 1 && this.product.selling_price > 0) {
-                    let price = this.product.selling_price - ((this.computeDiscount.value) / 100) * this.product.selling_price;
-                    return price;
-                }
-                else {
-                    let price = this.product.selling_price - this.computeDiscount.value;
-                    return price;
-                }
-            }
-            else {
-                return 0;
-            }
-        },
+        ...mapGetters(["getHostname", "getToken", "getUser", "getDefaultImage", "getDiscounts", "getCurrency", "getUserAdminID", "getMobile"]),
         computeExpiry() {
             if (this.product.expires && this.product.active == 0) {
                 return true;
@@ -116,6 +92,9 @@ li {
 hr{
     border-color: $dark-light;
     margin: 0 20px;
+        &:first-child{
+        visibility: hidden;
+    }
 }
 .button {
     padding: 10px;
@@ -124,7 +103,8 @@ hr{
 }
 .qty-input{
     width: 68px;
-    height: 45px;
+    height: 40px;
+    border-radius: 10px;
 }
 
 </style>
--- src/components/app/layouts/SelectionSheet.vue
+++ src/components/app/layouts/SelectionSheet.vue
@@ -1,29 +1,33 @@
 <template>
     <teleport to="#app">
-        <tertiary-backdrop @click.prevent="$store.commit('unSetSelectionSheet')" v-bind:zIndex="300"/>
-        <div class="product-selection-sheet" :class="computeWidth ? 'width-active': 'width-inactive'">
-            <div class="selection-wrap">
-                 <div class="selection-main">
-                    <div class="selection-header">
-                        <div class="header-holder">
-                            <div class="flex top-hold">
-                                <button @click.prevent="$store.commit('unSetSelectionSheet')" class="button button-secondary cancel-btn">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 23.062 19.844">
-                                        <path d="M3769.432-2903.1l-10.236-9.362,10.214-9.937a1,1,0,0,1,1.414.02,1,1,0,0,1-.02,1.413l-7.665,7.457H3781.3a.981.981,0,0,1,.961,1,.981.981,0,0,1-.961,1h-18.1l7.58,6.932a1,1,0,0,1,.063,1.412,1,1,0,0,1-.739.326A1,1,0,0,1,3769.432-2903.1Z" transform="translate(-3759.195 2922.683)" fill="#0e142c"></path>
-                                    </svg>
-                                </button>
-                                <div id="selection_title" class="flex head-title"></div>
-                                <div id="selection_button"></div>
+        <transition name="fade">
+            <tertiary-backdrop v-if="getSelectionSheet.active" @click.prevent="$store.commit('unSetSelectionSheet')" v-bind:zIndex="300"/>
+        </transition>
+        <transition :name="getMobile? 'slide' : ''">
+            <div v-if="getSelectionSheet.active" class="product-selection-sheet" :class="computeWidth ? 'width-active': 'width-inactive'">
+                <div class="selection-wrap">
+                    <div class="selection-main">
+                        <div class="selection-header">
+                            <div class="header-holder">
+                                <div class="flex top-hold">
+                                    <button @click.prevent="$store.commit('unSetSelectionSheet')" class="button button-secondary cancel-btn">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 23.062 19.844">
+                                            <path d="M3769.432-2903.1l-10.236-9.362,10.214-9.937a1,1,0,0,1,1.414.02,1,1,0,0,1-.02,1.413l-7.665,7.457H3781.3a.981.981,0,0,1,.961,1,.981.981,0,0,1-.961,1h-18.1l7.58,6.932a1,1,0,0,1,.063,1.412,1,1,0,0,1-.739.326A1,1,0,0,1,3769.432-2903.1Z" transform="translate(-3759.195 2922.683)" fill="#0e142c"></path>
+                                        </svg>
+                                    </button>
+                                    <div id="selection_title" class="flex head-title"></div>
+                                    <div id="selection_button"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="selection-body" :style="{height: (windowHeight-180)+'px'}">
-                        <div id="selection_sheet_searchInput"></div>
-                        <div id="selection_sheet_body"></div>
+                        <div class="selection-body" :style="{height: (windowHeight-180)+'px'}">
+                            <div id="selection_sheet_searchInput"></div>
+                            <div id="selection_sheet_body"></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </transition>
     </teleport>
 </template>
 <script>
@@ -34,7 +38,7 @@ export default {
     name: 'SelectionSheet',
     props: ['windowHeight'],
     computed: {
-        ...mapGetters(['getWindowWidth']),
+        ...mapGetters(['getWindowWidth', 'getSelectionSheet', 'getMobile']),
         computeWidth() {
             if(this.getWindowWidth <= 600) {
                 return true
@@ -61,8 +65,7 @@ export default {
         .selection-body{
             overflow-y: auto;
             padding: 0 20px 60px 20px;
-            min-height: 300px;
-            
+            min-height: 300px; 
         }
     }
     .selection-header{
@@ -102,8 +105,20 @@ export default {
         border-top-right-radius: 20px;
         border-top-left-radius: 20px;
     }
-    
 }
-
+@media screen and (max-width: 499px){
+    .selection-header, .selection-body{
+        padding-left: 15px !important;
+        padding-right: 15px !important;
+    }
+    .selection-body{
+        padding-left: 10px !important;
+        padding-right: 10px !important;
+    }
+}
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(820px);  
+}
 </style>
                                 
