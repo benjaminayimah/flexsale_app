0da820ec9c6a78292f9768be702a8b14f36c002a
Benjamin <benjaminayimah@gmail.com>
2022-08-14 00:31:52 +0000

    release beta 2.5

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ export default createStore({
     rememberToken: '',
     // OAuth: '617984689362-02931j85j49mm913mn3lf72j4njggajg.apps.googleusercontent.com',
     OAuth: '749226126008-jhs808q75ioafn76o109qcuh4peav4jl.apps.googleusercontent.com',
-    betaNumber: '2.2',
+    betaNumber: '2.5',
     windowHeight: '',
     windowWidth: '',
     bodyWidth: '',
